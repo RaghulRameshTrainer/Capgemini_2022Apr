ff778e63e5831c71ad7310f1663906c5f1f99a67
RaghulRameshTrainer <89928567+RaghulRameshTrainer@users.noreply.github.com>
2022-04-18 20:30:07 +0530

    Day5

--- circles.py
+++ circles.py
@@ -1,5 +1,9 @@
 import math
 def circle_area(r):
+    if r<0:
+        raise ValueError("The radius cannot be negative")
+    if type(r) not in [int,float]:
+        raise TypeError("The radius must be a non-negative real number!")
     return math.pi*(r**2)
 
 # radi=[2,0,-3,2+5j,True,"chennai"]
--- test_circles.py
+++ test_circles.py
@@ -8,6 +8,14 @@ class TestCircleArea(unittest.TestCase):
         self.assertAlmostEqual(circle_area(0),0)
         self.assertAlmostEqual(circle_area(2.1),pi*(2.1**2))
     def test_areas(self):
-        self.assertAlmostEqual(circle_area(2),pi)
+        self.assertAlmostEqual(circle_area(2),pi*4)
     def test_cricleareas(self):
         self.assertAlmostEqual(circle_area(10),pi*100)
+    def test_values(self):
+        self.assertRaises(ValueError,circle_area,-2)
+    def test_types(self):
+        self.assertRaises(TypeError,circle_area,2+3j)
+        self.assertRaises(TypeError, circle_area, True)
+        self.assertRaises(TypeError, circle_area, "Python")
+    def test_value(self):
+        self.assertNotEqual(circle_area(10),pi*4)
